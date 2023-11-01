15002d33f954b6fc648d1ae2b51c7f1f165ca021
root <mgiraudo@ies21.edu.ar>
2023-11-01 10:55:50 -0300

    inicio js

---
+++ generos.js
@@ -0,0 +1,14 @@
+const generos = {
+    museo: "Galeria Vanguardia Visual",
+    cuadros:[
+        {
+            imagen:"./multimedia/marat.jpg",
+            titulo:"La Morte di Marat",
+            autor:"David",
+            año:"1793",
+            
+        }
+    ]
+}
+
+export default generos
---
+++ principal.js
@@ -0,0 +1,9 @@
+import generos from "./generos.js"
+
+//Referencia
+const principal= document.getElementById("generos")
+
+principal.innerHTML= `${productos.tienda}`
+
+//Principal
+console.log(principal)
